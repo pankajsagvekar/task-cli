ce9e18e811ab73b3ca47957ba30748c5b1a13287
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-09-26 18:43:43 +0530

    add tasks option

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,21 @@
-public class Main {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
+@Command(name = "task-cli", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Task tracker is a project used to track and manage your tasks")
+public class Main implements Runnable {
+
+    @Option(names = {"add"}, description = "Used to add tasks")
+    private String tasks;
+
+    @Override
+    public void run() {
+        System.out.printf("Task Added Successfully: "+tasks);
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Main()).execute("add=buy a pen");
+        System.exit(exitCode);
+    }
 }
